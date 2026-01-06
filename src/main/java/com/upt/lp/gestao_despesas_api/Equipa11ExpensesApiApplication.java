c48491e4a5a4376ba55ce4050291ccf54e16218c
BrunaRodrigues0 <50410@alunos.upt.pt>
2026-01-06 11:10:52 +0000

    Initial commit

---
+++ src/main/java/com/upt/lp/gestao_despesas_api/Equipa11ExpensesApiApplication.java
@@ -0,0 +1,13 @@
+package com.upt.lp.gestao_despesas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Equipa11ExpensesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Equipa11ExpensesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upt/lp/gestao_despesas_api/Equipa11ExpensesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upt.lp.gestao_despesas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Equipa11ExpensesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
